95216f1f32223cb02dc12789b051cdf9c64a88ce
haekang <1003gorkd@naver.com>
2022-02-11 19:46:17 +0900

    파일구조 진짜 끝

---
+++ src/pages/Index.js
@@ -0,0 +1,7 @@
+import Detail from "./Detail";
+import Login from "./Login";
+import Main from "./main";
+import Post from "./Post";
+import Signup from "./Signup";
+
+export { Detail, Login, Main, Post, Signup };
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,18 +1,27 @@
-import "./App.css";
-import Container from "@mui/material/Container";
-
-import { Route } from "react-router-dom";
-import Main from "../pages/Main";
-import Header from "./Header";
 import React from "react";
+import { Route } from "react-router-dom";
+
+// material ui
+import { layout } from "./material";
+
+// page/index
+import { Detail, Login, Main, Post, Signup } from "../pages/Index";
+
+// shared/index
+import { Header } from "./Index";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <Container maxWidth="lg">
+      <layout.Container maxWidth="lg">
         <Route path="/" exact component={Main} />
-      </Container>
+        <Route path="/user/login" exact component={Login} />
+        <Route path="/user/signup" exact component={Signup} />
+        <Route path="/posts" exact component={Post} />
+        <Route path="/edit/:postId" exact component={Post} />
+        <Route path="/posts/list/:postId" exact component={Detail} />
+      </layout.Container>
     </React.Fragment>
   );
 }
