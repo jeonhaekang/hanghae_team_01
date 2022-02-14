7b9c0ab53905b1bf61d9e0ffa9fc3570c40889e7
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-02-14 17:39:35 +0900

    1

--- src/shared/Request.js
+++ src/shared/Request.js
@@ -1,17 +1,7 @@
 import { create } from "@mui/material/styles/createTransitions";
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://3.39.0.192",
-});
-
-const temp = axios.create({
-  withCredentials: true,
-  headers: {
-    "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    accept: "*/*",
-  },
-});
+const instance = axios.create({});
 
 instance.interceptors.request.use((config) => {
   config.headers["Content-Type"] =
@@ -22,7 +12,7 @@ instance.interceptors.request.use((config) => {
 const apis = {
   signup: (post) => instance.post("/user/signup", post),
   idCheck: (id) => instance.post("/user/idCheck", { username: id }),
-  login: (frm) => temp.post("/user/login", frm, { withCredentials: true }),
+  login: (frm) => instance.post("/user/login", frm, { withCredentials: true }),
 };
 
 export default apis;
