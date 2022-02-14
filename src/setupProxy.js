cea7b02aa98620af422a4bf06649d65c82b7fc91
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-02-14 17:17:33 +0900

    로그인 다시 성공

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,14 +2,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    // 불러오려는 server 의 api path
     "/user",
     createProxyMiddleware({
-      target: "http://3.39.0.192:8080", // server 주소를 넣어주면 된다.
+      target: "http://3.39.0.192:8080",
       changeOrigin: true,
-      pathRewrite: {
-        "^/user": "", // 하위 url 초기화
-      },
     })
   );
 };
